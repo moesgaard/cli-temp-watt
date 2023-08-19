15ef279a1fcb38882af3f9229207dc562db88c37
Morten Moesgaard <morten@moesgaards.dk>
2023-08-19 17:16:04 +0200

    Update temp-watt-watch.sh
    call sensors once and remove _ in names

--- temp/temp-watt-watch.sh
+++ temp/temp-watt-watch.sh
@@ -7,8 +7,9 @@ green=$(tput setaf 2)
 blue=$(tput setaf 4)
 red=$(tput setaf 1)
 normal=$(tput sgr0)
+RESULTS=$( sensors )
 echo -e "Watt usage:"
-BOB=$(sensors | grep -E "(SVI2_P_Core|SVI2_P_SoC|W)" )
+BOB=$( echo  -e  "$RESULTS" | grep -E "(SVI2_P_Core|SVI2_P_SoC|W)" )
 ZOBE=0
 for ill in $BOB
 do
@@ -16,74 +17,74 @@ do
 	VOV=""
 	BOBBY=$( echo $ill | awk '{print $1}' | sed 's/SVI2_P_/Watt /g' | sed 's/PPT/GPU Watt/g' | sed 's/://g' )
 	for ins in $( seq 0 $ZOB )
-	do 
+	do
 		VOV+=$BOF
 	done
 	
-	if [ $ZOB -le 79  ]; 
+	if [ $ZOB -le 79  ];
 	then
-		printf "%-10s\t%-10s\t%-80s\n" $BOBBY":"  "${green}$ZOB${normal}" $VOV 
+		printf "%-10s\t%-10s\t%-80s\n" $BOBBY":"  "${green}$ZOB${normal}" $VOV
 	
 	elif  [ $ZOB -ge 80   ];
 	then	
-		printf "%-10s\t%-10s\t%-80s\n" $BOBBY":"  "${blue}$ZOB${normal}" $VOV 
+		printf "%-10s\t%-10s\t%-80s\n" $BOBBY":"  "${blue}$ZOB${normal}" $VOV
 	
 	elif [ $ZOB -ge 120  ];
 	then
-		printf "%-10s\t%-10s\t%-80s\n" $BOBBY  "${red}$ZOB${normal}" $VOV 
+		printf "%-10s\t%-10s\t%-80s\n" $BOBBY  "${red}$ZOB${normal}" $VOV
 	fi
 
 
 	ZOBE="$ZOBE+$ZOB"
 done
-printf "%-10s\t%-8d\t%-10s\n" $( echo "Total Watt")  $( echo  "$ZOBE" | bc) 
+printf "%-10s\t%-8d\t%-10s\n" $( echo "Total Watt")  $( echo  "$ZOBE" | bc)
 
 ZOB=0
-BOB=$(sensors | grep "T" | grep -v "PPT" | grep -v "VTT" | grep -v "EC_"   )
+BOB=$(  echo  -e  "$RESULTS" | grep "T" | grep -v "PPT" | grep -v "VTT" | grep -v "EC_"   )
 echo -e "\nCPU:"
 for ill in $BOB
 do
 	ZOB=$( echo $ill | awk '{print $2}' | grep -Eo '[0-9]*\.[0-9]*' | cut -d. -f1 | xargs )
 	VOV=""
-	BOBBY=$( echo $ill | awk '{print $1}' )
+	BOBBY=$( echo $ill | awk '{print $1}'  | sed  "s/_/ /g"  )
 	for ins in $( seq 0 $ZOB )
-		do 
+		do
 		VOV+=$BOF
 	done
-	if [ $ZOB -le 55  ]; 
+	if [ $ZOB -le 55  ];
 	then
-		printf "%-10s\t%-5s\t%-80s\n" $BOBBY  "${green}$ZOB${normal}" $VOV 
+		printf "%-10s\t%-5s\t%-80s\n" $BOBBY  "${green}$ZOB${normal}" $VOV
 	elif  [ $ZOB -ge 56   ];
 	then	
-		printf "%-10s\t%-5s\t%-80s\n" $BOBBY  "${blue}$ZOB${normal}" $VOV 
+		printf "%-10s\t%-5s\t%-80s\n" $BOBBY  "${blue}$ZOB${normal}" $VOV
 	elif [ $ZOB -ge 71 ];
 	then
-		printf "%-10s\t%-5s\t%-80s\n" $BOBBY  "${red}$ZOB${normal}" $VOV 
+		printf "%-10s\t%-5s\t%-80s\n" $BOBBY  "${red}$ZOB${normal}" $VOV
 	fi
 done
 
 echo -e  "\nGPU:"
-BOB=$(sensors | grep -E  "(junction|edge|mem)")
+BOB=$(  echo  -e  "$RESULTS" | grep -E  "(junction|edge|mem)")
 for ill in $BOB
 do
 	ZOB=$( echo $ill | awk '{print $2}' | grep -E '[0-9]*\.[0-9]*' | cut -d. -f1  )
 	VOV=""
-	BOBBY=$( echo $ill | awk '{print $1}')
+	BOBBY=$( echo $ill | awk '{print $1}' | sed  "s/_/ /g" )
 	for ins in $( seq 0 $ZOB )
-	do 
+	do
 		VOV+=$BOF
 	done
 	printf "%-10s\t%-5d\t%-80s\n" $BOBBY  $ZOB   $VOV
 done
-echo -e  "\ntemps:"
-BOB=$(sensors | grep -E  "thermistor")
+echo -e  "\nTemperature:"
+BOB=$(  echo  -e  "$RESULTS" | grep -E  "sensor")
 for ill in $BOB
 do
 	ZOB=$( echo $ill | awk '{print $2}' | grep -E '[0-9]*\.[0-9]*' | cut -d. -f1  )
 	VOV=""
-	BOBBY=$( echo $ill | awk '{print $1}')
+	BOBBY=$( echo $ill | awk '{print $1}' | sed  "s/_/ /g")
 	for ins in $( seq 0 $ZOB )
-	do 
+	do
 		VOV+=$BOF
 	done
 	printf "%-10s\t%-5d\t%-80s\n" $BOBBY  $ZOB   $VOV
